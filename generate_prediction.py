9611eee7cd32faf0408638ec7cb451a0c1fd2086
Neil McBlane <neilmcblane@googlemail.com>
2020-09-22 21:50:22 +0100

    Simplify to script

--- generate_prediction.py
+++ generate_prediction.py
@@ -7,7 +7,7 @@ difficulty rating (long term value).
 
 Example
 -------
-ToDo(neilmcb): an example should go here, of the form::
+TODO(neilmcb): an example should go here, of the form::
 
     $ python generate_prediction.py argv_1 argv_2
 
@@ -24,20 +24,6 @@ for a given team. It takes into account factors such as historical results,
 form and various Opta performance metrics. It is used as a proxy for odds
 for long term planning as odds are only available a couple of gameweeks in
 advance.
+
+TODO(neilmcb): implement in a single script, refactor afterwards.
 """
-from src.genetic_algorithm import Population
-
-ARGV_N_GENERATIONS = 1000
-"""int: The number of generations to evolve the population through.
-
-ToDo(neilmcb): placeholder value, move to command line argument.
-"""
-
-population = Population()
-ancestors = []
-
-for _ in range(ARGV_N_GENERATIONS):
-    new_population = population.breed().mutate()
-    # Store old populations to track evolution with time
-    ancestors.append(population)
-    population = new_population
